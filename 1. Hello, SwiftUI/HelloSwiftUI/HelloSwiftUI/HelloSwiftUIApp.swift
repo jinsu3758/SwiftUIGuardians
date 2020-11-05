c70666b5b7056612eb39f7592d6650553bf44e76
jinsu3758 <jinsu3758@naver.com>
2020-11-05 10:43:08 +0900

    init

---
+++ 1. Hello, SwiftUI/HelloSwiftUI/HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by 박진수 on 2020/11/05.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
