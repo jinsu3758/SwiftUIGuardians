1657be5a06e1c6e5c093c45824c3b40cba174045
jinsu3758 <jinsu3758@naver.com>
2020-11-06 21:17:21 +0900

    Memo App init

---
+++ 2. SwiftUI Memo/SwiftUIMemo/SwiftUIMemo/MemoListScene.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIMemo
+//
+//  Created by 박진수 on 2020/11/06.
+//
+
+import SwiftUI
+
+struct MemoListScene: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct MemoListScene_Previews: PreviewProvider {
+    static var previews: some View {
+        MemoListScene()
+    }
+}
