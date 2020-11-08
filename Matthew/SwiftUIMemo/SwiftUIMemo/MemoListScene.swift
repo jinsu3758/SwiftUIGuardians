e5e881c658886ae9db7b379f798e3bfa824e82c0
devming <cmk330@gmail.com>
2020-11-08 22:55:56 +0900

    Swift Memo Step1

---
+++ Matthew/SwiftUIMemo/SwiftUIMemo/MemoListScene.swift
@@ -0,0 +1,21 @@
+//
+//  MemoListScene.swift
+//  SwiftUIMemo
+//
+//  Created by devming on 2020/11/08.
+//
+
+import SwiftUI
+
+struct MemoListScene: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct MemoListScene_Previews: PreviewProvider {
+    static var previews: some View {
+        MemoListScene()
+    }
+}
