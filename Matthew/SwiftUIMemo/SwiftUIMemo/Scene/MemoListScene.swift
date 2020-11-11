13f62d6fa6070b90e0e8eb6e74343fe2bc566e2e
devming <cmk330@gmail.com>
2020-11-12 01:25:07 +0900

    Step5, Step6

--- Matthew/SwiftUIMemo/SwiftUIMemo/Scene/MemoListScene.swift
+++ Matthew/SwiftUIMemo/SwiftUIMemo/Scene/MemoListScene.swift
@@ -9,20 +9,11 @@ import SwiftUI
 
 struct MemoListScene: View {
     @EnvironmentObject var store: MemoStore
-    @EnvironmentObject var formatter: DateFormatter
     
     var body: some View {
         NavigationView {
             List(store.list) { memo in
-                VStack(alignment: .leading) {
-                    Text(memo.content)
-                        .font(.body)
-                        .lineLimit(1)
-                    
-                    Text("\(memo.insertDate, formatter: self.formatter)")
-                        .font(.caption)
-                        .foregroundColor(.secondary)
-                }
+                MemoCell(memo: memo)
             }
             .navigationBarTitle("내 메모")
         }
@@ -36,3 +27,4 @@ struct MemoListScene_Previews: PreviewProvider {
             .environmentObject(DateFormatter.memoDateFormatter)
     }
 }
+
