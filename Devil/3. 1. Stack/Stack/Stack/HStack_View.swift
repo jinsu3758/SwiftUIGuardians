efb5f203bb1d52a383f5956e7a944b0d41ccad63
jinsu3758 <jinsu3758@naver.com>
2021-01-13 14:13:56 +0900

    devil : HStack도 Spacer() 넣어서 aligment 바꿔보기

--- Devil/3. 1. Stack/Stack/Stack/HStack_View.swift
+++ Devil/3. 1. Stack/Stack/Stack/HStack_View.swift
@@ -9,27 +9,33 @@ import SwiftUI
 
 struct HStack_View: View {
     var body: some View {
-        HStack(alignment: .bottom, spacing: 0) {
-            Image(systemName: "suit.heart.fill")
-               .resizable()
-               .frame(width: 50, height: 50)
-               .foregroundColor(.red)
-            Image(systemName: "suit.club.fill")
-               .resizable()
-               .frame(width: 100, height: 100)
-               .foregroundColor(.black)
+        
+        VStack(alignment: .leading) {
+            Spacer()
+            
+            HStack(alignment: .bottom, spacing: 0) {
+                Image(systemName: "suit.heart.fill")
+                   .resizable()
+                   .frame(width: 50, height: 50)
+                   .foregroundColor(.red)
+                Image(systemName: "suit.club.fill")
+                   .resizable()
+                   .frame(width: 100, height: 100)
+                   .foregroundColor(.black)
 
-            Image(systemName: "suit.spade.fill")
-               .resizable()
-               .frame(width: 50, height: 50)
-               .foregroundColor(.black)
+                Image(systemName: "suit.spade.fill")
+                   .resizable()
+                   .frame(width: 50, height: 50)
+                   .foregroundColor(.black)
 
-            Image(systemName: "suit.diamond.fill")
-               .resizable()
-               .frame(width: 50, height: 50)
-               .foregroundColor(.red)
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                Image(systemName: "suit.diamond.fill")
+                   .resizable()
+                   .frame(width: 50, height: 50)
+                   .foregroundColor(.red)
+            }
+            
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+        
 //        .frame(width: 300, height: 300)
         
         
